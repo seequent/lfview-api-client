6ca26278844e9281e60360892148aeb3ddad8a22
Yunus Can Emre <yunus.emre@seequent.com>
2019-06-21 12:06:54 -0600

    track lfview-resources-spatial

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         'requests',
         'properties[full]>=0.5.6',
         'lfview-resources-files==0.0.2',
-        'lfview-resources-spatial==0.0.3',
+        'lfview-resources-spatial==0.0.4b0',
         'lfview-resources-manifests==0.0.1',
         'lfview-resources-scene==0.0.1',
         'futures; python_version == "2.7"'
