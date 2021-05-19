0f5692a0435e8bebc121d9d999dc7398dac9823c
Franklin Koch <franklin.koch@seequent.com>
2021-05-19 11:28:07 -0600

    Add view_to_omf functionality

--- lfview/client/convert/__init__.py
+++ lfview/client/convert/__init__.py
@@ -1,5 +1,5 @@
 """Conversion functions to build Views"""
 from __future__ import absolute_import
 
-from .omf import omf_to_view
+from .omf import omf_to_view, view_to_omf
 from .steno3d import steno3d_to_view
--- setup.py
+++ setup.py
@@ -26,8 +26,10 @@ setuptools.setup(
     install_requires=[
         'requests',
         'properties[full]>=0.5.6',
+        'omf==1.0.1',
+        'steno3d',
         'lfview-resources-files==0.0.2',
-        'lfview-resources-spatial==0.0.3',
+        'lfview-resources-spatial==0.0.5',
         'lfview-resources-manifests==0.0.1',
         'lfview-resources-scene==0.0.1',
         'futures; python_version == "2.7"'
